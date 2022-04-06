91b20d857c7234195e98c0ab00c3bc7af3ebaa48
Jacob <Jacobshortall@gmail.com>
2022-04-06 23:36:16 +0100

    Create products app & add models

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', related_name='products', null=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50)
+    content = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(blank=True)
