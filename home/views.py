1959a8e4c7388cea2bd99f4a357f99cf86f51621
Jacob <Jacobshortall@gmail.com>
2022-04-06 17:47:31 +0100

    Add epic to home page

--- home/views.py
+++ home/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home/index.html', {
-        'iter': range(20),
-    })
+    return render(request, 'home/index.html')
