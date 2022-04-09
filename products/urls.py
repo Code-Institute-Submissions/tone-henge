1560d550b4af52614e1ec806cf645f06ac14de68
Jacob <Jacobshortall@gmail.com>
2022-04-09 01:04:44 +0100

    Render new product_view template and link up product previews

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.products, name='products'),
+    path('product-view/<int:product_id>/', views.product_view, name='product_view')
 ]
--- products/views.py
+++ products/views.py
@@ -8,3 +8,11 @@ def products(request):
     context = {'products': products, }
 
     return render(request, 'products/products.html', context)
+
+
+def product_view(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    context = {'product': product, }
+
+    return render(request, 'products/product_view.html', context)
