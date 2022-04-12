914c3fe6c45d87abefcd68dad90d49013a94867d
Jacob <Jacobshortall@gmail.com>
2022-04-12 20:10:18 +0100

    Display count of results, checking if there has been a search

--- products/views.py
+++ products/views.py
@@ -5,20 +5,21 @@ from .models import Product
 
 def products(request):
     products = Product.objects.all()
+    search_term = None
 
     if request.GET:
         if 'q' in request.GET:
-            query = request.GET['q']
+            search_term = request.GET['q']
 
-            queries = Q(name__icontains=query) | Q(
-                content__icontains=query) | Q(category__name__icontains=query)
+            queries = Q(name__icontains=search_term) | Q(
+                content__icontains=search_term) | Q(category__name__icontains=search_term)
             products = products.filter(queries)
 
         if 'category' in request.GET:
             category = request.GET['category'].replace('+', ' ')
             products = Product.objects.filter(category__name__iexact=category)
 
-    context = {'products': products, }
+    context = {'products': products, 'search_term': search_term, }
 
     return render(request, 'products/products.html', context)
 
