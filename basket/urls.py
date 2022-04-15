6f45582ec649d0b26d28c844d6ab9bd83da13730
Jacob <Jacobshortall@gmail.com>
2022-04-15 19:47:14 +0100

    Implement add to bag functionality

--- basket/urls.py
+++ basket/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.basket, name='basket')
+    path('', views.basket, name='basket'),
+    path('<product_id>', views.add_to_basket, name='add_to_basket')
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def basket(request):
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, product_id):
+    if request.method == 'POST':
+        quantity = int(request.POST['quantity'])
+        basket = request.session.get('basket', {})
+        url = request.POST['redirect_url']
+
+        if product_id in basket:
+            basket[product_id] += quantity
+        else:
+            basket[product_id] = quantity
+
+        request.session['basket'] = basket
+        print(request.session['basket'])
+
+        return redirect(url)
