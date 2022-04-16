8fbbf288edc9c9605111a317a094cce07c798f04
Jacob <Jacobshortall@gmail.com>
2022-04-16 17:57:18 +0100

    Update context processor to retrieve session data

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,7 +1,21 @@
+from products.models import Product
+
+
 def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for product_id, quantity in basket.items():
+        product = Product.objects.get(pk=product_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'product': product,
+            'product_id': product_id,
+            'quantity': quantity,
+        })
 
     context = {
         'basket_items': basket_items,
