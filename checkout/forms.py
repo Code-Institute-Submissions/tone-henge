9c868fa7119a8774e166ae6e9387276247b399e4
Jacob <Jacobshortall@gmail.com>
2022-04-18 12:44:03 +0100

    Add further customisation to order form

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,10 +3,20 @@ from .models import Order
 
 
 class OrderForm(ModelForm):
+    """
+    Create form and customise fields to be used in checkout template
+    to collect delivery information.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         for field in self.fields:
+            if self.fields[field].required:
+                self.fields[field].label += '*'
             self.fields[field].widget.attrs.update({'class': 'form-control'})
+
+        self.fields['phone_id'].label = 'Phone Number'
 
     class Meta:
         model = Order
