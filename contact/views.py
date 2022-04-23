446a5f73f17f253a8f947872c5f2fb7f8ee67a65
Jacob <Jacobshortall@gmail.com>
2022-04-23 13:43:34 +0100

    Migrate contact to new template

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def contact(request):
-    return HttpResponse('test render')
+    return render(request, 'contact/contact.html')
