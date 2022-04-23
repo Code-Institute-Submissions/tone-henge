1525c1c3b04f1337a3a443065de758603e8c5a95
Jacob <Jacobshortall@gmail.com>
2022-04-23 14:23:35 +0100

    Begin handling POST requests in contact view

--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import UserQueryForm
+from django.contrib import messages
 
 
 def contact(request):
     """Render contact page and handle form submission for user queries."""
 
     form = UserQueryForm()
+
+    if request.method == 'POST':
+        form = UserQueryForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.add_message(
+                request, messages.SUCCESS, 'Thank you. Your query has been sent successfully.')
+
+            return redirect('contact')
 
     context = {'form': form, }
 
