52e5c2b9d1417f6090e971bca8255a460cb2b7f0
Jacob <Jacobshortall@gmail.com>
2022-04-24 22:11:58 +0100

    Add basic functionality for adding to wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import WishlistItem
+from products.models import Product
 
 
 @login_required
@@ -7,3 +10,15 @@ def wishlist(request):
     """Render user's wishlist page."""
 
     return render(request, 'wishlist/wishlist.html')
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """Add item to user's wishlist."""
+
+    product = get_object_or_404(Product, pk=product_id)
+    wishlist_item = WishlistItem(user=request.user, product=product)
+    wishlist_item.save()
+
+    messages.add_message(request, messages.SUCCESS, 'Added to wishlist.')
+    return redirect('product_view', product_id)
