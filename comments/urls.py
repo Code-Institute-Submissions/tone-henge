f3bddce877ea10f62d4077d6cf91edc346994a48
Jacob <Jacobshortall@gmail.com>
2022-04-24 15:32:10 +0100

    Add delete_comment view

--- comments/urls.py
+++ comments/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add_comment/<product_id>', views.add_comment, name='add_comment'),
+    path('delete_comment/<comment_id>',
+         views.delete_comment, name='delete_comment'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CommentForm
+from .forms import CommentForm, Comment
 from products.models import Product
 
 
 @login_required
 def add_comment(request, product_id):
-    """Add comment to specific post."""
+    """Add comment to specific post. Ensures user is auth'd to do so."""
 
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -28,4 +28,24 @@ def add_comment(request, product_id):
 
     messages.add_message(request, messages.WARNING,
                          'Error. Something went wrong with your request.')
+    return redirect('home')
+
+
+@login_required
+def delete_comment(request, comment_id):
+    """Delete comment from specific post. Ensures user is auth'd to do so."""
+
+    if request.method == 'POST':
+        comment = get_object_or_404(Comment, pk=comment_id)
+        url = request.POST['redirect_url']
+
+        if request.user == comment.author:
+            comment.delete()
+            messages.add_message(request, messages.SUCCESS, 'Comment deleted.')
+
+            return redirect(url)
+
+    messages.add_message(request, messages.WARNING,
+                         'Error. Something went wrong with your request.')
+
     return redirect('home')
